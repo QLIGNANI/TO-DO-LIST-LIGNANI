0b5624f5bd407547a90b3e854bd159cd580e2b25
Quentin Lignani <qlignani@gmail.com>
2023-01-07 13:58:30 +0100

    Add Modal CRUD
    Ajout - Modification - Suppression des tâches

---
+++ components/Modal.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import useOutsideClick from '@/hooks/useOutsideClick';
+import styles from '@/styles/todolist.module.css'
+
+const Modal = (props) => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const handleClose = () => {
+        if (isOpen) {
+            setIsOpen(false);
+
+            setTimeout(() => {
+                if (props.isModalOpen) props.onClose();
+            }, 50);
+        }
+    };
+
+    const modalRef = useOutsideClick(handleClose);
+
+    return (
+        <div
+            className={`${styles.modalContainer}
+      ${isOpen ? 'bg-gray-900 backdrop-filter backdrop-blur-sm' : 'bg-transparent backdrop-filter backdrop-blur-none'}`}
+        >
+            <div
+                ref={modalRef}
+                className={`${styles.modal} ${props.class}`}
+            >
+                {props.component}
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
