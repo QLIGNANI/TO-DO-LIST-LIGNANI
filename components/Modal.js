5f5e617111e86b416868de0009b9e780cf072383
Quentin Lignani <qlignani@gmail.com>
2023-01-07 14:26:53 +0100

    Delete setTimeout

--- components/Modal.js
+++ components/Modal.js
@@ -9,9 +9,6 @@ const Modal = (props) => {
         if (isOpen) {
             setIsOpen(false);
 
-            setTimeout(() => {
-                if (props.isModalOpen) props.onClose();
-            }, 50);
         }
     };
 
@@ -20,7 +17,7 @@ const Modal = (props) => {
     return (
         <div
             className={`${styles.modalContainer}
-      ${isOpen ? 'bg-gray-900 backdrop-filter backdrop-blur-sm' : 'bg-transparent backdrop-filter backdrop-blur-none'}`}
+      ${isOpen ? 'bg-gray-700 backdrop-filter backdrop-blur-sm' : 'bg-transparent backdrop-filter backdrop-blur-none'}`}
         >
             <div
                 ref={modalRef}
